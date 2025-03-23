04ab5e0d7ea97942c513691a320d351eb1ebfb06
LvvNikolas <lvvniko7@gmail.com>
2025-03-23 11:36:06 -0700

    added some changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Экспорт для статического сайта
+  distDir: 'out', // Указываем папку вывода
 };
 
 export default nextConfig;
