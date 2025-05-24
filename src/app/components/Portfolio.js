a5d0330071c1ffbc1ff0342d7c32e76d1e26c548
Nikolai Orlovsky <lvvniko7@gmail.com>
2025-05-24 15:54:15 -0700

    added some stuff

--- src/app/components/Portfolio.js
+++ src/app/components/Portfolio.js
@@ -1,26 +1,27 @@
-// src/app/components/Portfolio.js
 import Link from 'next/link';
-import { FaAndroid, FaApple } from 'react-icons/fa'; // Импортируем иконки
-// import styles from '../page.module.css';
-import styles from '../styles/Portfolio.module.css'; // Импортируем стили для портфолио
+import { FaAndroid, FaApple } from 'react-icons/fa';
+import styles from '../styles/Portfolio.module.css';
 
 export default function Portfolio() {
+  const projects = [
+    { name: 'Lungs test exercise' },
+    { name: 'Engineering IQ Test' },
+    { name: 'Math Game' },
+    { name: 'Six Pack in 30 Days' },
+    { name: 'Push Ups - Fitness Trainer' },
+    { name: 'Legs Workout - 4 Week Program' },
+    { name: '10+ Android Utility Apps' },
+  ];
+
   return (
     <section id="portfolio" className={styles.portfolioSection}>
       <div className={styles.portfolioHeader}>
         <h2>Our Projects</h2>
         <p className={styles.portfolioSubtitle}>7 years of expertise</p>
       </div>
+
       <ul className={styles.projectList}>
-        {[
-          { name: 'Lungs test exercise' },
-          { name: 'Engineering IQ Test' },
-          { name: 'Math Game' },
-          {name: 'Six Pack in 30 Days'},
-          {name:"Push Ups - Fitness Trainer"},
-          {name:"Legs workout - 4 Week Program"},
-          { name: '10+ Android Utility Apps' },
-        ].map((project, index) => (
+        {projects.map((project, index) => (
           <li key={index} className={styles.projectItem}>
             <a href="#" className={styles.projectLink}>
               <span>{project.name}</span>
@@ -32,6 +33,7 @@ export default function Portfolio() {
           </li>
         ))}
       </ul>
+
       <div className={styles.privacyPolicy}>
         <Link href="/privacy-policy" className={styles.privacyLink}>
           Privacy Policy
