0d1473ca4275823a85ea8d13e7826dfbaa6da5bb
LvvNikolas <lvvniko7@gmail.com>
2025-05-25 19:18:32 -0700

    changed Contact section

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -1,6 +1,6 @@
-// src/app/components/ContactSection.js
-import { FaEnvelope, FaPhone } from 'react-icons/fa'; // Импорт иконок
-import styles from '../styles/Contact.module.css'; // Импортируем стили для контактов
+import { FaEnvelope, FaPhone, FaTelegramPlane } from 'react-icons/fa';
+import styles from '../styles/Contact.module.css';
+
 export default function Contact() {
   return (
     <section id="contact" className={styles.contactSection}>
@@ -8,13 +8,17 @@ export default function Contact() {
         <h2>Contact Us</h2>
         <p>We’re always open to communication!</p>
         <div className={styles.contactInfo}>
-          <a href="mailto:contact@kata.com" className={styles.contactItem}>
+          <a href="mailto:lvvkatatech@gmail.com" className={styles.contactItem}>
             <FaEnvelope className={styles.contactIcon} />
             <span>lvvkatatech@gmail.com</span>
           </a>
           <a href="tel:+15623309823" className={styles.contactItem}>
             <FaPhone className={styles.contactIcon} />
             <span>+1 562 330 9823</span>
+          </a>
+          <a href="#" className={styles.contactItem}>
+            <FaTelegramPlane className={styles.contactIcon} />
+            <span>Telegram</span>
           </a>
         </div>
       </div>
